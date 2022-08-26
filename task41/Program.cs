b9a0fb6196ea5c9635f6d23b6d0ba3cbbcf56e8d
«Anastasia» <muzalkova.anastasia@gmail.com>
2022-08-26 08:55:44 +0300

    add all

---
+++ task41/Program.cs
@@ -0,0 +1,27 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine ("Введите количество чисел: ");
+int size = Convert.ToInt32 (Console.ReadLine ());
+
+int [] array = new int [size];
+int i = 0;
+for (i = 0; i < array.Length; i++)
+{
+    Console.WriteLine ("Введите элемент массива:");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write (string.Join(", ", array));
+int count = 0;
+for (i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine ($"В массиве {count} положительных чисел");
+
---
+++ task43/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine ("Введите первое значение для первой прямой: ");
+double b1 = Convert.ToDouble (Console.ReadLine ());
+Console.WriteLine ("Введите второе значение для первой прямой: ");
+double k1 = Convert.ToDouble (Console.ReadLine ());
+Console.WriteLine ("Введите первое значение для второй прямой: ");
+double b2 = Convert.ToDouble (Console.ReadLine ());
+Console.WriteLine ("Введите первое значение для второй прямой: ");
+double k2 = Convert.ToDouble (Console.ReadLine ());
+
+if (k1 == k2 && b1 != b2)
+{
+    Console.WriteLine("Прямые не пересекаются");
+}
+if (k1 == k2 && b1 == b2 )
+{
+    Console.WriteLine ("Прямые совпадают");
+}
+double x = (b2 - b1)/(k1 - k2);
+double y1 = k1 * x + b1;
+double y2 = k2 * x + b2;
+if (y1 == y2)
+{
+    Console.WriteLine($"Координаты пересечения прямых равны ({x};{y1})");
+}
+// Console.WriteLine (x);
+// Console.WriteLine (y1);
+// Console.WriteLine (y2);
